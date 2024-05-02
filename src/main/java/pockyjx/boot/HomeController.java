2e771268df609cb71d109be9cba00e445c5a5713
pockyjx <0127828b@gmail.com>
2024-05-02 15:30:05 +0900

    init project

---
+++ src/main/java/pockyjx/boot/HomeController.java
@@ -0,0 +1,13 @@
+package pockyjx.boot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	@GetMapping("/hello")
+	public String home() {
+		return "hello";
+	}
+
+}
